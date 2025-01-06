8536f3307d640a88c0dbf35dacb6cbe69f37238e
Can Men <can.men@campus.tu-berlin.de>
2025-01-06 16:57:39 +0300

    Modified Board.hs file

--- src/Board.hs
+++ src/Board.hs
@@ -4,6 +4,7 @@ module Board where  -- do NOT CHANGE export of module
 -- Note: Imports allowed that DO NOT REQUIRE TO ANY CHANGES TO package.yaml, e.g.:
 --       import Data.Chars
 import Data.Char
+import Debug.Trace
 
 -- #############################################################################
 -- ############# GIVEN IMPLEMENTATION                           ################
@@ -38,6 +39,13 @@ instance Eq Cell where
 -- ################## - 2 Functional Points                   ###################
 -- ################## - 1 Coverage Point                      ###################
 -- #############################################################################
+
+validateFEN :: String -> Bool
+validateFEN str
+  | null str = False
+  | otherwise =
+      let parts = splitOn '/' str
+      in length parts == 10 && all ((`correctRow` 10) . handleEmpty) parts
 --AI was used to implement the splitOn function properly 
 splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn _ [] = [[]]
@@ -47,25 +55,20 @@ splitOn delim (x:xs)
   where
     rest = splitOn delim xs
 correctRow :: String -> Int -> Bool
-correctRow row x=
-    let validRow=map divideFENChar row
-    in all (>0) validRow && sum validRow==x
+correctRow row x
+  | row == "10" = True  
+  | otherwise =
+      let validRow = map divideFENChar row
+      in sum validRow == x  
 --AI was used to create a function that will read the integers in a string as integers and to count letters in the parts
 divideFENChar :: Char -> Int
 divideFENChar c
   | isDigit c = read [c]
   | c `elem` "bBwWgG" = 1
-  | otherwise = 0
+  | otherwise =0
 handleEmpty :: String -> String
-handleEmpty "" = "0"  -- 
+handleEmpty "" = "10"  -- 
 handleEmpty row = row
-validateFEN :: String -> Bool
-validateFEN str
-  | null str = False
-  | otherwise =
-      let parts = splitOn '/' str
-      in length parts == 10 && all ((`correctRow` 10) . handleEmpty) parts
-
 
 -- ##############################################################################
 -- ################## IMPLEMENT buildBoard :: String -> Board ###################
@@ -74,8 +77,11 @@ validateFEN str
 -- ##############################################################################
 
 buildBoard :: String -> Board
-buildBoard board = map divideRow (splitOn '/' board)
+buildBoard board = map handleEmptyRow (splitOn '/' board)
 
+handleEmptyRow :: String -> [Cell]
+handleEmptyRow "" = replicate 10 Empty
+handleEmptyRow row = divideRow row
 -- AI was used to create the recursion correctly
 divideRow :: String -> [Cell]
 divideRow [] = []
@@ -91,4 +97,4 @@ charToCell 'w' = Soldier White
 charToCell 'W' = Flag White
 charToCell 'g' = General White
 charToCell 'G' = General Black
-charToCell _   = Empty 
+charToCell_ = Empty
