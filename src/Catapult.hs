2428b8544be98cb3b005768ee3d9785590b603c3
Can Men <can.men@campus.tu-berlin.de>
2025-01-07 01:44:21 +0300

    soldierMoves function test

--- src/Catapult.hs
+++ src/Catapult.hs
@@ -7,6 +7,7 @@ import Board
 import Data.Char
 import Data.Maybe
 import Debug.Trace
+import Data.List (nub)
 data Move = Move {start :: Pos, target :: Pos}
 
 instance Show Move where
@@ -23,6 +24,7 @@ instance Eq Move where
 -- #################################################################################################
 
 data Catapult = N | NE | E | SE | S | SW | W | NW deriving Show
+data Direction = Straight | Leftway | Rightway
 
 flagMoves :: Board -> Player -> [Move]
 flagMoves board player
@@ -53,10 +55,18 @@ isBlank board (Pos column1 row1)=
   --Function to get the wanted cell 
 getCell :: Board -> Pos -> Cell
 --AI was used to correctly enumerate the char section
-getCell board (Pos col row)= (board !! row) !! numericalColumn col
+getCell board (Pos col row)
+  | row < 0 || row >= length board = Empty  -- Check if the row index is out of bounds
+  | colIdx < 0 || colIdx >= length (head board) = Empty  -- Check if the column index is out of bounds
+  | otherwise = (board !! row) !! colIdx
+  where
+    colIdx = numericalColumn col
+    
 numericalColumn :: Char -> Int
 numericalColumn column = fromEnum column - fromEnum 'a'
-
+-- Column number to original value
+numberToChar :: Int -> Char
+numberToChar number= toEnum (number + fromEnum 'a')
 
 
 
@@ -89,9 +99,7 @@ isGeneral board player (Pos col row) =
     General p -> p == player
     _ -> False
 
--- Column number to original value
-numberToChar :: Int -> Char
-numberToChar number= toEnum (number + fromEnum 'a')
+
 
 -- AI Helper function
 isValidMove :: Board -> Pos -> Bool
@@ -105,7 +113,102 @@ isValidMove board (Pos col row) = isBlank board (Pos col row)
 -- #################################################################################################
 
 soldierMoves :: Board -> Player -> Pos -> [Move]
-soldierMoves board player (Pos col row)=[]
+soldierMoves board player (Pos col row)
+  | not (isSoldier board player (Pos col row))=[]
+  | otherwise=nub (
+    normalMoves board player (Pos col row) ++
+    attackMoves board player (Pos col row) ++
+    retreatMoves board player (Pos col row))
+--Control the soldier
+isSoldier :: Board -> Player -> Pos -> Bool
+isSoldier board player (Pos col row)=
+  case getCell board (Pos col row) of
+    Soldier p -> p == player
+    _ -> False
+--Classic moves
+normalMoves :: Board -> Player -> Pos -> [Move]
+normalMoves board player (Pos col row)=
+  let direction = if player == White then 1 else -1
+      colLeft = numericalColumn col - 1
+      colRight = numericalColumn col + 1
+  in [Move (Pos col row) (Pos col (row - direction))
+      | isValidMove board (Pos col (row - direction)), (row - direction) >= 0, (row - direction) <= 9] ++ -- Forward Move
+     [Move (Pos col row) (Pos (numberToChar colLeft) row)
+      | colLeft >= 0, colLeft <= 9, isValidMove board (Pos (numberToChar colLeft) row)] ++ -- Left Move
+     [Move (Pos col row) (Pos (numberToChar colRight) row)
+      | colRight >= 0, colRight <= 9, isValidMove board (Pos (numberToChar colRight) row)] -- Right Move
+--Takeout moves
+attackMoves :: Board -> Player -> Pos -> [Move]
+attackMoves board player (Pos col row)=
+  let direction= if player==White then 1 else -1
+  in [Move (Pos col row) (Pos colChar row1)|colNum <- [numericalColumn col - 1 .. numericalColumn col
+                                                    + 1],
+         colNum >= 0,
+         colNum <= 9,row1 <- [row - direction],
+         row1 >= 0,
+         row1 <= 9,
+         let colChar = numberToChar colNum,
+         isEnemy board player (Pos colChar row1)]
+--Flee Moves
+retreatMoves :: Board -> Player -> Pos -> [Move]
+retreatMoves board player (Pos col row)
+  | not (isUnderThreat board player (Pos col row))=[]
+  | otherwise= retreatMovesBackwards board player (Pos col row)
+--Is the soldier under any threat
+isUnderThreat:: Board -> Player -> Pos -> Bool
+isUnderThreat board player (Pos col row)=
+  let direction = if player == White then 1 else -1
+      forward = Pos col (row - direction)
+      diagonalLeft = Pos (numberToChar (numericalColumn col-1)) (row - direction)
+      diagonalRight = Pos (numberToChar (numericalColumn col+1)) (row - direction)
+  in any (isEnemy board player) [forward, diagonalLeft, diagonalRight]
+
+retreatMovesBackwards:: Board -> Player -> Pos -> [Move]
+retreatMovesBackwards board player (Pos col row)=
+  let direction= if player==White then 1 else -1
+  in [Move (Pos col row) (Pos col (row + direction*2))
+      | isBlank board (Pos col (row + direction*2)),
+      (row+direction*2)>=0,
+      (row+direction*2)<=9,
+      isPathClear board player (Pos col row) Straight] ++ --Backwards 2 steps
+      [Move (Pos col row) (Pos (numberToChar (numericalColumn col-2)) (row + direction*2))|
+         numericalColumn col-2 >= 0,
+         numericalColumn col-2 <= 9,
+         isPathClear board player (Pos col row) Leftway]++ --Left diagonal retreat 2 steps
+      [Move (Pos col row) (Pos (numberToChar (numericalColumn col+2)) (row + direction*2))|
+         numericalColumn col+2 >= 0,
+         numericalColumn col+2 <= 9,
+         isPathClear board player (Pos col row) Rightway]   --Right diagonal retreat 2 steps
+--AI was used to implement this function properly
+isPathClear :: Board -> Player -> Pos -> Direction -> Bool
+isPathClear board player (Pos col row) direction =
+  case direction of
+    Straight ->
+      all (isValidMove board) [Pos col r | r <- [row + offset, row + offset * 2]] -- Controlling the 2 steps behind
+    Leftway -> --Controlling 2 steps left diagonal
+      let colLeft = numericalColumn col - 1
+          colLeftTwo = numericalColumn col - 2
+      in colLeft >= 0 && colLeftTwo >= 0 &&
+         all (isValidMove board)
+         [Pos (numberToChar colLeft) (row + offset),
+          Pos (numberToChar colLeftTwo) (row + offset * 2)]
+    Rightway -> --Controlling 2 steps right diagonal
+      let colRight = numericalColumn col + 1
+          colRightTwo = numericalColumn col + 2
+      in colRight <= 9 && colRightTwo <= 9 &&
+         all (isValidMove board)
+         [Pos (numberToChar colRight) (row + offset),
+          Pos (numberToChar colRightTwo) (row + offset * 2)]
+  where
+    offset = if player == White then 1 else -1
+--Checks if a cell is enemy or not
+isEnemy :: Board -> Player -> Pos -> Bool
+isEnemy board player pos =
+  case getCell board pos of
+    Soldier p -> p /= player
+    General p -> p /= player
+    Flag p -> p /= player
+    _ -> False
 -- #################################################################################################
 -- ################## IMPLEMENT catapultMoves :: Board -> Player -> Pos -> [Move]  ###################
 -- ################## - 4 Functional Points                                      ###################
@@ -114,7 +217,7 @@ soldierMoves board player (Pos col row)=[]
 
 catapultMoves :: Board -> Player -> Pos -> [Move]
 catapultMoves board player (Pos col row)=[]
-  
+
 
 
 -- #################################################################################################
@@ -124,13 +227,13 @@ catapultMoves board player (Pos col row)=[]
 -- #################################################################################################
 
 playerWon :: Board -> Player -> Bool
-playerWon board player=
-  let opponent = if player == White then Black else White
+playerWon board player=False
+  {-let opponent = if player == White then Black else White
       --Testing all of the possibilities
       hasOpponentFlag = any (elem (Flag opponent)) board
       hasOpponentGeneral = any (elem (General opponent)) board
       opponentHasMoves = not (null (listMoves board opponent))
-  in not (hasOpponentFlag && hasOpponentGeneral) || not opponentHasMoves
+  in not (hasOpponentFlag && hasOpponentGeneral) || not opponentHasMoves-}
 
 -- #################################################################################################
 -- ################## IMPLEMENT listMoves :: Board -> Player -> [Move]           ###################
--- test/Spec.hs
+++ test/Spec.hs
@@ -61,10 +61,6 @@ main = hspec $ do
             let board = buildBoard "/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/1bbbbbbbb1"
             let moves = flagMoves board White
             not (null moves) `shouldBe` False
-        it "flagMoves with all positions filled with obstacles for White" $ do
-            let board = buildBoard "wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww"
-            let moves = flagMoves board White
-            moves `shouldBe` []
         it "calculates valid flag moves for a player Black" $ do
             let board = buildBoard "/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
             let moves = flagMoves board Black
@@ -135,7 +131,110 @@ main = hspec $ do
             let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
             let moves = generalMoves board Black (Pos 'j' 9)
             not (null moves) `shouldBe` False
-        
 
-    
+    describe "soldierMoves Function tests" $ do
+        it "calculates valid white soldier moves" $ do
+            let board = buildBoard "/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1//3w6"
+            let moves = soldierMoves board White (Pos 'd' 9)
+            not (null moves) `shouldBe` True
+        it "calculates invalid white soldier moves corner" $ do
+            let board = buildBoard "gwwwwwwwww/ww1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/wwwwwwwwww/wwwwwwwwww"
+            let moves = soldierMoves board White (Pos 'j' 9)
+            not (null moves) `shouldBe` False
+        it "calculates invalid white soldier moves middle" $ do
+            let board = buildBoard "gwwwwwwwww/ww1w1w1w1w/1w1w1w1w1w/wwwwwwwwww/wwwwwwwwww/wwwwwwwwww/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board White (Pos 'f' 4)
+            not (null moves) `shouldBe` False
+        it "wrong soldier white" $ do
+            let board = buildBoard "bwwwwwwwww/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board White (Pos 'a' 0)
+            not (null moves) `shouldBe` False
+        it "general instead of soldier white" $ do
+            let board = buildBoard "g9/111w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board White (Pos 'a' 0)
+            not (null moves) `shouldBe` False
+        it "empty instead of soldier white" $ do
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board White (Pos 'a' 0)
+            not (null moves) `shouldBe` False
+        it "white soldier can takeout and can escape from every way" $ do --New tests begin here
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5///"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout and can escape from diagonals way" $ do 
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5/4w5/4w5/4w5"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout and can escape from just straight way" $ do 
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5/bbbb1bbbbb/bbbb1bbbbb/bbbb1bbbbb"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout cant escape (not enough spaces)" $ do
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/bbbbbbbbbb/1w8"
+            let moves = soldierMoves board White (Pos 'b' 9)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout cant escape (first tile obstructed second free)" $ do  
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5/wwwwwwwwww//"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout cant escape (first tile free second obstructed)" $ do 
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5//wwwwwwwwww/"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "white soldier can takeout cant escape (both tiles obstructed)" $ do 
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/4w5/wwwwwwwwww/wwwwwwwwww/"
+            let moves = soldierMoves board White (Pos 'e' 6)
+            not (null moves) `shouldBe` True
+        it "calculates valid black soldier moves" $ do
+            let board = buildBoard "1b8//1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1//3b6"
+            let moves = soldierMoves board Black (Pos 'd' 9)
+            not (null moves) `shouldBe` True
+        it "calculates invalid black soldier moves corner" $ do
+            let board = buildBoard "bbbbbbbbbb/bbbbbbbbbb/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/wwwwwwwwww/wwwwwwwwww"
+            let moves = soldierMoves board Black (Pos 'a' 0)
+            not (null moves) `shouldBe` False
+        it "calculates invalid black soldier moves middle" $ do
+            let board = buildBoard "gwwwwwwwww/ww1w1w1w1w/1w1w1w1w1w/bbbbbbbbbb/bbbbbbbbbb/bbbbbbbbbb/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board Black (Pos 'f' 4)
+            not (null moves) `shouldBe` False
+        it "wrong soldier black" $ do
+            let board = buildBoard "1w8/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board Black (Pos 'b' 0)
+            not (null moves) `shouldBe` False
+        it "general instead of soldier black" $ do
+            let board = buildBoard "g9/111w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/1G8"
+            let moves = soldierMoves board Black (Pos 'b' 9)
+            not (null moves) `shouldBe` False
+        it "empty instead of soldier black" $ do
+            let board = buildBoard "/3w1w1w1w/1w1w1w1w1w/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/"
+            let moves = soldierMoves board Black (Pos 'b' 9)
+            not (null moves) `shouldBe` False
+        it "black soldier can takeout and can escape from every way" $ do --New tests begin here
+            let board = buildBoard "///4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout and can escape from diagonals way" $ do 
+            let board = buildBoard "4b5/4b5/4b5/4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout and can escape from just straight way" $ do 
+            let board = buildBoard "bbbb1bbbbb/bbbb1bbbbb/bbbb1bbbbb/4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout cant escape (not enough spaces)" $ do
+            let board = buildBoard "/1b8/wwwwwwwww/1w1w1w1w1w/5g4/4G5/b1b1b1b1b1/b1b1b1b1b1/bbbbbbbbbb/1w8"
+            let moves = soldierMoves board Black (Pos 'b' 1)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout cant escape (first tile obstructed second free)" $ do  
+            let board = buildBoard "//bbbbbbbbbb/4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout cant escape (first tile free second obstructed)" $ do 
+            let board = buildBoard "/bbbbbbbbbb//4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
+        it "black soldier can takeout cant escape (both tiles obstructed)" $ do 
+            let board = buildBoard "/bbbbbbbbbb/bbbbbbbbbb/4b5/wwwwwwwwww/4G5/4w5///"
+            let moves = soldierMoves board Black (Pos 'e' 3)
+            not (null moves) `shouldBe` True
     
